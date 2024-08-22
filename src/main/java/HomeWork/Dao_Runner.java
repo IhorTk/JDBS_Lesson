8daa2f4df1a57e5daffe82f6ec6904d2a16ed96f
IgorTkachenko <igortkachenko987@gmail.com>
2024-08-22 16:32:37 +0200

    start

--- src/main/java/HomeWork/Dao_Runner.java
+++ src/main/java/HomeWork/Dao_Runner.java
@@ -3,23 +3,58 @@ package HomeWork;
 import HomeWork.dao.InvoiceDao;
 import HomeWork.entity.Invoice;
 
+import java.io.FilterOutputStream;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public class Dao_Runner {
     public static void main(String[] args) {
         InvoiceDao invoiceDao = InvoiceDao.getInstance();
-//        List<Invoice> allInvoices = invoiceDao.findAll();
-//
-//        allInvoices.forEach(invoice -> System.out.println(invoice));
 
-        Invoice invoice = new Invoice(23,"333-33-33", 2,
+        List<Invoice> allInvoices = invoiceDao.findAll();
+
+        allInvoices.forEach(invoice -> System.out.println(invoice));
+
+        Invoice invoice = new Invoice(22,"333-33-33", 2,
                 BigDecimal.valueOf(222.22),BigDecimal.valueOf(222.22),
                 LocalDate.of(2024,02,19),
                 LocalDate.of(2024,05,25),
                 LocalDate.of(2024,04,01));
         invoiceDao.save(invoice);
+
+        System.out.println("**************************************************************************");
+
+        Optional<Invoice>  findInvoice = invoiceDao.findById(22);
+        System.out.println(findInvoice);
+
+        System.out.println("**************************************************************************");
+
+        Invoice invoice1 = new Invoice(22,"555-55-55", 2 ,
+                BigDecimal.valueOf(444.44),BigDecimal.valueOf(555.55), LocalDate.of(2023,03,25),LocalDate.of(2023,05,
+                06),LocalDate.of(2023,07,31));
+        invoiceDao.update(invoice1);
+
+        System.out.println("**************************************************************************");
+
+        findInvoice = invoiceDao.findById(22);
+        System.out.println(findInvoice);
+
+        System.out.println("**************************************************************************");
+
+
+        invoiceDao.deleteById(22);
+
+
+        List<Invoice> allInvoices1 = invoiceDao.findAll();
+        allInvoices1.forEach(invoice2 -> System.out.println(invoice2));
+
+        System.out.println("**************************************************************************");
+
+
+
+
 
 
     }
--- src/main/java/HomeWork/dao/InvoiceDao.java
+++ src/main/java/HomeWork/dao/InvoiceDao.java
@@ -9,11 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class InvoiceDao implements Dao<Integer, Invoice>{
+public class InvoiceDao implements Dao<Integer, Invoice> {
 
     private static final InvoiceDao INSTANCE = new InvoiceDao();
 
-    private static final String FIND_ALL_SQL= """
+    public static InvoiceDao getInstance() {
+        return INSTANCE;
+    }
+
+    private static final String FIND_ALL_SQL = """
             SELECT *
             FROM invoices;
             """;
@@ -23,7 +27,30 @@ public class InvoiceDao implements Dao<Integer, Invoice>{
             VALUES(?,?,?,?,?,?,?,?); 
             """;
 
-    private InvoiceDao(){
+    private static final String UPDATE_SQL = """
+            UPDATE invoices
+            SET number =?,
+                client_id = ?,
+                invoice_total =?,
+                payment_total =?,
+                invoice_date =?,
+                due_date =?,
+                payment_date =?
+            WHERE invoice_id=?;
+            """;
+
+    private static final String DELETE_SQL = """
+            DELETE FROM invoices
+            WHERE invoice_id=?;
+            """;
+
+    private static final String FIND_BY_ID = """
+            SELECT *
+            FROM invoices
+            WHERE invoice_id=?;
+            """;
+
+    private InvoiceDao() {
 
     }
 
@@ -34,7 +61,7 @@ public class InvoiceDao implements Dao<Integer, Invoice>{
             ResultSet resultSet = preparedStatement.executeQuery();
             Invoice invoice = null;
             List<Invoice> invoices = new ArrayList<>();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 invoice = new Invoice(
                         resultSet.getInt(1),
                         resultSet.getString(2),
@@ -59,14 +86,14 @@ public class InvoiceDao implements Dao<Integer, Invoice>{
     public void save(Invoice entity) {
         try (Connection connection = ConnectionManager.openConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(SAVE_SQL)) {
-            preparedStatement.setInt(1,entity.getInvoiceId());
-            preparedStatement.setString(2,entity.getNumber());
-            preparedStatement.setInt(3,entity.getClientId());
-            preparedStatement.setBigDecimal(4,entity.getInvoiceTotal());
-            preparedStatement.setBigDecimal(5,entity.getPaymentTotal());
+            preparedStatement.setInt(1, entity.getInvoiceId());
+            preparedStatement.setString(2, entity.getNumber());
+            preparedStatement.setInt(3, entity.getClientId());
+            preparedStatement.setBigDecimal(4, entity.getInvoiceTotal());
+            preparedStatement.setBigDecimal(5, entity.getPaymentTotal());
             preparedStatement.setDate(6, Date.valueOf(entity.getInvoiceDate()));
-            preparedStatement.setDate(7,Date.valueOf(entity.getDueDate()));
-            preparedStatement.setDate(8,Date.valueOf(entity.getPaymentDate()));
+            preparedStatement.setDate(7, Date.valueOf(entity.getDueDate()));
+            preparedStatement.setDate(8, Date.valueOf(entity.getPaymentDate()));
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
@@ -76,19 +103,61 @@ public class InvoiceDao implements Dao<Integer, Invoice>{
 
     @Override
     public void update(Invoice entity) {
+        try (Connection connection = ConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
+            preparedStatement.setString(1, entity.getNumber());
+            preparedStatement.setInt(2, entity.getClientId());
+            preparedStatement.setBigDecimal(3, entity.getInvoiceTotal());
+            preparedStatement.setBigDecimal(4, entity.getPaymentTotal());
+            preparedStatement.setDate(5, Date.valueOf(entity.getInvoiceDate()));
+            preparedStatement.setDate(6, Date.valueOf(entity.getDueDate()));
+            preparedStatement.setDate(7, Date.valueOf(entity.getPaymentDate()));
+            preparedStatement.setInt(8, entity.getInvoiceId());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteById(Integer id) {
+        try (Connection connection = ConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_SQL)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     @Override
-    public Optional<Invoice> findById(Integer id) {
-        return Optional.empty();
+    public Optional<Invoice> findById(Integer invoice_id) {
+        try (Connection connection = ConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID)) {
+            preparedStatement.setInt(1, invoice_id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Invoice invoice = null;
+            if (resultSet.next()) {
+                invoice = new Invoice(
+                        resultSet.getInt("invoice_id"),
+                        resultSet.getString("number"),
+                        resultSet.getInt("client_id"),
+                        resultSet.getBigDecimal("invoice_total"),
+                        resultSet.getBigDecimal("payment_total"),
+                        resultSet.getObject("invoice_date", LocalDate.class),
+                        resultSet.getObject("due_date", LocalDate.class),
+                        resultSet.getObject("payment_date", LocalDate.class)
+                );
+
+            }
+            return Optional.ofNullable(invoice);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static InvoiceDao getInstance() {
-        return INSTANCE;
-    }
 }
